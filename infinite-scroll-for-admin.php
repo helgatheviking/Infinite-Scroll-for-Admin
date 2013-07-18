25eec4bdd428fee27a18fa065577754c48b4d640
Kathy Darling <helgatheviking@gmail.com>
2013-07-18 12:30:02 -0400

    intial commit

---
+++ infinite-scroll-for-admin.php
@@ -0,0 +1,122 @@
+<?php
+/*
+Plugin Name: Infinite Scroll for Admin
+Plugin URI: http://www.kathyisawesome.com/449/nav-menu-roles/
+Description: Use infinite scroll in the WordPress backend
+Version: 1.0
+Author: Kathy Darling
+Author URI: http://www.kathyisawesome.com
+License: GPL2
+
+    Copyright 2013  Kathy Darling  (email: kathy.darling@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+
+// don't load directly
+if ( ! function_exists( 'is_admin' ) ) {
+    header( 'Status: 403 Forbidden' );
+    header( 'HTTP/1.1 403 Forbidden' );
+    exit();
+}
+
+/**
+ * Make translation ready
+ *
+ * @since 1.0b
+ * @return void
+ */
+function infinite_scroll_for_admin_text_domain() {
+    load_plugin_textdomain( 'infinite-scroll-for-admin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+    }
+add_action( 'admin_init', 'infinite_scroll_for_admin_text_domain' );
+
+/**
+ * Load infinite scroll on particular pages
+ *
+ * @since 1.0b
+ * @return void
+ */
+
+function infinite_scroll_for_admin_scripts( $hook ) {
+
+
+    if ( in_array( $hook, array( 'edit.php', 'edit-tags.php', 'edit-comments.php' ) ) ) {
+
+        $min = ( defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ) ? '' : '.min';
+
+        wp_enqueue_script( 'infinite_scroll', plugins_url( 'js/jquery.infinitescroll' . $min . '.js' , __FILE__ ), array('jquery'), '2.0b2', true );
+
+        $l10n = array( 'msgText' => __( 'Loading...', 'infinite-scroll-for-admin' ),
+                        'finishedMsg' => __( 'The end.', 'infinite-scroll-for-admin' ) );
+
+        wp_localize_script( 'infinite_scroll', 'Infinite_Scroll_Admin', $l10n );
+
+        add_action( 'admin_print_footer_scripts', 'infinite_scroll_for_admin_init_scripts' );
+
+    }
+
+}
+add_action( 'admin_enqueue_scripts', 'infinite_scroll_for_admin_scripts' );
+
+/**
+ * Initiate infinite scroll on particular pages
+ *
+ * @since 1.0b
+ * @return void
+ */
+
+function infinite_scroll_for_admin_init_scripts(){ ?>
+
+    <style>
+    #infscr-loading { text-align: center; }
+    </style>
+
+    <script type="text/javascript">
+
+    (function ($) {
+
+        // little conditional so we can re-use script on both edit.php and edit-comments.php
+        if ( $('#the-list').length ){
+            var scrollme = '#the-list';
+        } else if ( $('#the-comment-list').length ) {
+            scrollme = '#the-comment-list';
+        }
+
+        var colspan = $( scrollme + ' tr:first-child').find('td').length;
+        var contentSelector = scrollme;
+        var itemSelector = scrollme + ' > tr';
+
+       $(scrollme).infinitescroll({
+            loading: {
+                finishedMsg: '<em>' + Infinite_Scroll_Admin.finishedMsg + '</em>',
+                msg: $('<tr id="infscr-loading"><td colspan="' + colspan + '"><img alt="Loading..." src="' + "<?php echo admin_url( 'images/wpspin_light.gif' ); ?>" + '" /><div>' + Infinite_Scroll_Admin.msgText + '</div></td></tr>')
+            },
+            nextSelector: ".pagination-links a.next-page",
+            navSelector: ".pagination-links",
+            contentSelector: contentSelector,
+            itemSelector: itemSelector,
+            prefill: true,
+            maxPage: $('.paging-input .total-pages').html(),
+            debug: true
+        });
+
+    })(jQuery)
+
+    </script>
+<?php
+}
+
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Infinite Scroll for Admin ===
+Contributors: helgatheviking
+Donate link: Donate link: https://inspirepay.com/pay/helgatheviking ‎
+Tags: admin
+Requires at least: 3.5.2
+Tested up to: 3.5.2
+Stable tag: 1.0b
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Apply the [Infinite Scroll](http://www.infinite-scroll.com/) plugin to WordPress admin screens, specifically edit.php and edit-comments.php.  No more paginating through the list of posts, pages (including custom post types) or comments!
+
+This is a totally beta plugin, so your mileage may vary.
+
+== Changelog ==
+
+= 1.0b =
+* Initial Release
